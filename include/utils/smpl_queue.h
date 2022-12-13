b8f6d3a9bdb6ab4118d99f090369c01ab11a5c10
Sebastian Rueth <sebastian.rueth@protonmail.com>
2022-12-13 18:15:18 +0100

    implement simple linked list queue

---
+++ include/utils/smpl_queue.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct SQueueNode {
+    void* data;
+    struct SQueueNode* next;
+    struct SQueueNode* prev;
+} SQueueNode;
+
+typedef struct SQueue {
+    // Size of one item in bytes
+    size_t item_size;
+
+    // The node that is at the front of the queue
+    // this will be handled next
+    struct SQueueNode* head;
+
+    // The node that is at the back of the queue
+    // new items come after the current tail
+    struct SQueueNode* tail;
+} SQueue;
+
+// Creates an empty queue
+SQueue squeue_create(size_t item_size);
+
+// Copies the item onto another node
+void squeue_push(SQueue* q, void* item);
+
+// Returns the next item in the queue
+//
+// This pointer is valid until the next call to pop
+void* squeue_pop_front(SQueue* q);
+
+// Destroys all items in the queue and itself
+void squeue_destroy(SQueue* q);
+
+// Count number of items in the queue
+size_t squeue_length(SQueue* q);
---
+++ src/utils/smpl_queue.c
@@ -0,0 +1,77 @@
+#include "utils/smpl_queue.h"
+
+#include <string.h>
+
+SQueue squeue_create(size_t item_size) {
+    SQueue s = {0};
+    s.item_size = item_size;
+    s.head = NULL;
+    s.tail = NULL;
+    return s;
+}
+
+void squeue_push(SQueue* q, void* item) {
+    SQueueNode* next_back_node = calloc(1, sizeof(SQueueNode));
+
+    // Copy the item into the new item's data pointer memory
+    next_back_node->data = calloc(1, q->item_size);
+    memcpy(next_back_node->data, item, q->item_size);
+
+
+    if(q->tail == NULL && q->head == NULL) {
+        q->tail = next_back_node;
+        q->head = next_back_node;
+    } else {
+        // The new node's previous is the old tail
+        next_back_node->prev = q->tail;
+
+        // The old tail's next node is the new node
+        q->tail->next = next_back_node;
+
+        // The new tail is the new node
+        q->tail = next_back_node;
+    }
+}
+
+void* squeue_pop_front(SQueue* q) {
+    static void* item_cache = NULL;
+    if(item_cache == NULL) {
+        item_cache = calloc(1, q->item_size);
+    }
+
+    if(q->head == NULL) {
+        return NULL;
+    }
+
+    memcpy(item_cache, q->head->data, q->item_size);
+
+    SQueueNode* old_head = q->head;
+    q->head = q->head->next;
+
+    free(old_head->data);
+    free(old_head);
+    return item_cache;
+}
+
+void squeue_destroy(SQueue* q) {
+    SQueueNode* head = q->head;
+    while(head != NULL) {
+        SQueueNode* this_head = head;
+
+        head = head->next;
+
+        free(this_head->data);
+        free(this_head);
+    }
+    memset(q, 0, sizeof(SQueue));
+}
+
+size_t squeue_length(SQueue* q) {
+    size_t len = 0;
+    SQueueNode* cur_node = q->head;
+    for(len = 0; cur_node != NULL; len++) {
+        cur_node = cur_node->next;
+    }
+
+    return len;
+}
